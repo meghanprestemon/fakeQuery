d8e3eeaa09dd268ec67a5f449e5300d20f7a31bd
Meghan Prestemon <m.m.hares@gmail.com>
2017-05-03 11:03:38 -0700

    done

--- fakeQuery.js
+++ fakeQuery.js
@@ -1,7 +1,7 @@
 var $ = function(selector) {
   if (selector.startsWith('#')) {
-
+    return document.getElementById(selector.substr(1));
   } else if (selector.startsWith('.')) {
-
+    return document.getElementsByClassName(selector.substr(1));
   }
 };
